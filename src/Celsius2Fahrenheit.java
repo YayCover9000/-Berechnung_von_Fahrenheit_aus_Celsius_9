c61d9a6053aa59a7350a40cf7c1d71617baa1fa8
YayCover9000 <yassuanf@googlemail.com>
2023-03-04 16:21:14 +0100

    now all basics are pushed

---
+++ src/Celsius2Fahrenheit.java
@@ -0,0 +1,10 @@
+public class Celsius2Fahrenheit {
+    public static void berechneCelsius2Fahrenheit(int celsius[], int fahrenheit[]) {
+        // Cast Celsius to double
+        // put double celsius in double fahrenheit array
+
+    }
+    public static void ausgeben(double fahrenheit[]) {
+
+    }
+}
