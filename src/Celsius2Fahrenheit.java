be108f7958f5a6f929e4e3d11c1e020df343fca0
YayCover9000 <yassuanf@googlemail.com>
2023-03-05 22:50:32 +0100

    took away celsius.length -1

--- src/Celsius2Fahrenheit.java
+++ src/Celsius2Fahrenheit.java
@@ -2,14 +2,14 @@ public class Celsius2Fahrenheit {
     public static void berechneCelsius2Fahrenheit(int celsius[], int fahrenheit[]) {
         // 1Fahrenheit=1,8∗Celsius+32
         // Initialise double fahrenheitDouble[]
-        double fahrenheitDouble[] = new double[celsius.length -1];
+        double fahrenheitDouble[] = new double[celsius.length];
         for (double m : fahrenheitDouble) {
             fahrenheitDouble[(int) m] = (1.8 * celsius[(int) m] + 32);
         }
-        // Test out for berechneCelsius2Fahrenheit[]
+        /* // Test out for berechneCelsius2Fahrenheit[]
         for (double i : fahrenheitDouble) {
             System.out.println("fahrenheitDouble: " + fahrenheitDouble[(int)i]);
-        }
+        } */
         Celsius2Fahrenheit.ausgeben(fahrenheitDouble);
 
 
