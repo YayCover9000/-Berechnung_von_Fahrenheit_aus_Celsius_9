48dd9486a2058ab3ae96288f8097888a592a33b1
YayCover9000 <yassuanf@googlemail.com>
2023-03-05 22:48:53 +0100

    changed foreach from int m to double m

--- src/Celsius2Fahrenheit.java
+++ src/Celsius2Fahrenheit.java
@@ -2,9 +2,9 @@ public class Celsius2Fahrenheit {
     public static void berechneCelsius2Fahrenheit(int celsius[], int fahrenheit[]) {
         // 1Fahrenheit=1,8∗Celsius+32
         // Initialise double fahrenheitDouble[]
-        double fahrenheitDouble[] = new double[celsius.length];
-        for (int m : celsius) {
-            fahrenheitDouble[m] = (1.8 * celsius[m] + 32);
+        double fahrenheitDouble[] = new double[celsius.length -1];
+        for (double m : fahrenheitDouble) {
+            fahrenheitDouble[(int) m] = (1.8 * celsius[(int) m] + 32);
         }
         // Test out for berechneCelsius2Fahrenheit[]
         for (double i : fahrenheitDouble) {
