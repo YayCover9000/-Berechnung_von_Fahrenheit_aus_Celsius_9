edee0bd1f43c42e151e7c3afc26950e365d0a825
YayCover9000 <yassuanf@googlemail.com>
2023-03-06 13:55:58 +0100

    Code Beautification

--- src/Celsius2Fahrenheit.java
+++ src/Celsius2Fahrenheit.java
@@ -1,31 +1,16 @@
 public class Celsius2Fahrenheit {
     public static void berechneCelsius2Fahrenheit(int[] celsius, int[] fahrenheit) {
-        // 1Fahrenheit=1,8∗Celsius+32
+        // Fahrenheit = 1,8∗Celsius+32
         // Initialise double fahrenheitDouble[]
         double fahrenheitDouble[] = new double[celsius.length];
         for (int m = 0; m < fahrenheitDouble.length; m++) {
             fahrenheitDouble[m] = (1.8 * celsius[m] + 32);
-            System.out.println("Dinge");
         }
-        for (double d : fahrenheitDouble) {
-            System.out.println(d);
-        }
-        System.out.println("Methode Berechnung Endet hier");
-        /* // Test out for berechneCelsius2Fahrenheit[]
-        for (double i : fahrenheitDouble) {
-            System.out.println("fahrenheitDouble: " + fahrenheitDouble[(int)i]);
-        } */
         Celsius2Fahrenheit.ausgeben(fahrenheitDouble);
-
-
-
-
-
     }
     public static void ausgeben(double fahrenheit[]) {
         for (double a : fahrenheit) {
-            System.out.println(a);
+            System.out.println("Fahrenheit: " + a);
         }
-        System.out.println("Methode ausgeben Endet hier");
     }
 }
--- src/Celsius2FahrenheitTesttreiber.java
+++ src/Celsius2FahrenheitTesttreiber.java
@@ -3,57 +3,27 @@ import java.util.Scanner;
 import java.lang.Math;
 public class Celsius2FahrenheitTesttreiber {
     public static void main(String [] args) {
-
         // Aufruf Testtreiber
         Celsius2FahrenheitTesttreiber.Celsius2FahrenheitTesttreiber();
-        System.out.println("Bis hier ausgabe Testtreiber");
-
-
-
     }
     public static void Celsius2FahrenheitTesttreiber() {
         // Initialisiere Scanner
         Scanner scanner = new Scanner(System.in);
         // Initialisiere Anzahl an Celsius werten
-        int anzahlCelsiusWerte; //TODO mit random Math generieren NICHT mit Scanner einlesen
+        int anzahlCelsiusWerte;
         // Lese n für Anzahl an Celsius werten ein
+        System.out.println("Zahl eingeben um Array größe zu determinieren: ");
         anzahlCelsiusWerte  = scanner.nextInt();
-        // anzahlCelsiusWert wird eine random Zahl zugewiesen
-        System.out.println("Math.random: " + anzahlCelsiusWerte);
         // Initialisierung int celsius[] und weise Anzahl an plätzen zu, welche mit scanner anzahlCelsiusWerten eingelesen wird
         int celsius[] = new int[anzahlCelsiusWerte];
         // Initialisierung int fahrenheit[] mit selber Größe wie celsius[]
         int fahrenheit[] = new int[anzahlCelsiusWerte];
-        // Initialisierung double fahrenheit[]
-        double fahrenheitDouble[] = new double[anzahlCelsiusWerte];
-        /* // Variable double für Math.random
-        double zwischenspeicherCelsius[] = new double[0]; */
-        // Array celsius befüllen
+        // celsius[] befüllen mit random werten aus Math.random
         for (int r = 0; r < celsius.length; r++) {
             celsius[r] = (int) (Math.random() * 100);
-            System.out.println("looop: " + celsius[r]);
+            System.out.println("Eingelesene Celsiuswerte: " + celsius[r]);
         }
-        System.out.println("celsius länge: " + celsius.length);
-        for (int c : celsius) {
-            System.out.println(c);
-        }
-        System.out.println("fahrenheit länge: " + fahrenheit.length);
-
         // übergabe Arrays an Methode berechneCelsius2Fahrenheit
         Celsius2Fahrenheit.berechneCelsius2Fahrenheit(celsius, fahrenheit);
-        // Erhalte, double fahrenheit[] zurück //TODO wie erhalte ich einen Wert aus der anderen Klasse und Methode zurück wenn die Methode void ist
-
-        // Übergebe, double fahrenheit[] an ausgeben
-        //Celsius2Fahrenheit.ausgeben(fahrenheitDouble);
-
-
-        /* int celsiusTesttreiber[] = new int[3];
-        int fahrenheitTesttreiber[] = new int[3];
-        for (int l : celsiusTesttreiber) {
-            celsiusTesttreiber[l] = (int) Math.random();
-        }
-        double fahrenheitDoubleTesttreiber[] = new double[3];
-        Celsius2Fahrenheit.berechneCelsius2Fahrenheit(celsiusTesttreiber, fahrenheitTesttreiber); */
     }
-
 }
